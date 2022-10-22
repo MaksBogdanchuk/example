3aefad9e484024e8790b33ab786c06dddedd563d
MaksimBogdanchuk <maks.bogdanchuk@gmail.com>
2022-10-22 14:02:28 +0700

    example2 random sum

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = 10;
-int numberB = 8;
+﻿int numberA = new Random().Next(1, 10);//1,2,3,4,5,6,7,8,9
+int numberB = new Random().Next(1, 10);//1,2,3,4,5,6,7,8,9
 int result = numberA + numberB;
 Console.WriteLine(result);
