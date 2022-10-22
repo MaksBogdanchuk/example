385c6deeb28c3a08511a287315d6d0528bcf6f1e
MaksimBogdanchuk <maks.bogdanchuk@gmail.com>
2022-10-22 16:38:00 +0700

    Ready Task004

--- HomeWork1/Task004/Program.cs
+++ HomeWork1/Task004/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C: ");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = A;
+
+if (B > max)
+{
+    max = B;
+}
+if (C > max)
+{
+    max = C;
+}
+
+Console.WriteLine("Макисмальное из трех чисел " + max);
